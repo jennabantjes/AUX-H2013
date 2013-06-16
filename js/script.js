70f46610be3322d6f3f867f8f335cb2f21676da5
Jenna Bantjes <jennabantjes@gmail.com>
2013-06-16 19:58:17 -0400

    Add search feature

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 (function (){
-  var friends = {
 
+  var friends = {
   "addressBook" : [
-    {
-      "name": "Nadia",
-      "email": "nadia@example.com",
-    },
-    {
-      "name": "Peyton",
-      "email": "peyton@example.com",
-    },
     {
       "name": "Ashley",
       "email": "ashley@example.com",
@@ -17,44 +9,115 @@
     {
       "name": "Becca",
       "email": "becca@example.com",
+    },
+    {
+      "name": "Chad",
+      "email": "chad@example.com",
+    },
+    {
+      "name": "Dan O",
+      "email": "dano@example.com",
+    },
+    {
+      "name": "Eve",
+      "email": "eve@example.com",
+    },
+    {
+      "name": "Francesca",
+      "email": "francesca@example.com",
+    },
+    {
+      "name": "Gary",
+      "email": "gary@example.com",
+    },
+    {
+      "name": "Holly",
+      "email": "holly@example.com",
+    },
+    {
+      "name": "Ines",
+      "email": "ines@example.com",
+    },
+    {
+      "name": "Josh",
+      "email": "josh@example.com",
+    },
+    {
+      "name": "Kyle",
+      "email": "kyle@example.com",
+    },
+    {
+      "name": "Lauren",
+      "email": "lauren@example.com",
+    },
+    {
+      "name": "Mike",
+      "email": "mike@example.com",
+    },
+    {
+      "name": "Nadia",
+      "email": "nadia@example.com",
+    },
+    {
+      "name": "Orion",
+      "email": "orion@example.com",
+    },
+    {
+      "name": "Peyton",
+      "email": "peyton@example.com",
+    },
+    {
+      "name": "Quincy",
+      "email": "quincy@example.com",
+    },
+    {
+      "name": "Rachel",
+      "email": "rachel@example.com",
+    },
+    {
+      "name": "Simon",
+      "email": "simon@example.com",
+    },
+    {
+      "name": "Teri",
+      "email": "teri@example.com",
     }
   ]
 }
 
-  var object = friends.addressBook,
-      friendsCount = object.length,
-      target = document.getElementsByTagName("body")[0],
-      i;
+var searchForm = document.getElementById("search-form"),
+    searchField = document.getElementById("q"),
+    count = friends.addressBook.length,
+    target = document.getElementById("output");
 
-      if(friendsCount > 0) {
-        for (i = 0; i < friendsCount; i = i + 1) {
-          var item = object[i],
-              name = item.name,
-              email = item.email;
+var address = {
 
-          target.innerHTML += '<p><a href="mailto'+ email +'">' + name + '</a></p>';
-        }
-      }
-}());
+  search : function(event){
 
+    var searchValue = searchField.value,
+        i;
 
-/* var adr = {
-
-  getAllContacts : function(){
-
-    var target = document.getElementById("output");
-    var book = friends.addressBook;
-    var count = book.length;
-    var i;
+    event.preventDefault();
 
     target.innerHTML = "";
 
-    if(count > 0){
-      for(i = 0; i < count; i = i + 1) {
-        var obj = book[i],
+    if(count > 0 && searchValue !== "") {
 
-        target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>'
+      for(i = 0; i < count; i = i + 1) {
+
+        var obj = friends.addressBook[i],
+            isItFound = obj.name.indexOf(searchValue);
+
+            if(isItFound !== -1) {
+
+              target.innerHTML += '<p> <a href="mailto:' + obj.email + '">'+ obj.name +'</a></p>'
+;
+        }
       }
     }
   }
-} */
+}
+
+searchField.addEventListener("keyup", address.search, false);
+
+}());
